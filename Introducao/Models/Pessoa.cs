90cbe0c38e8826e9c67937fe794d2da5949e7997
Jonathan Santos <jonathan2010.10@hotmail.com>
2021-09-08 15:53:40 -0300

    Add project files.

---
+++ Introducao/Models/Pessoa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Introducao.Models
+{
+    public class Pessoa
+    {
+        public int pessoaId { get; set; }
+        public int nomePessoa { get; set; }
+        public int tipoPessoa { get; set; }
+    }
+}
---
+++ Introducao/Models/Usuario.cs
@@ -0,0 +1,13 @@
+﻿namespace Introducao.Models
+{
+    public class Usuario
+    {
+        public string nomeUsuario { get; set; }
+        public string observacoes { get; set; }
+        public int idade { get; set; }
+        public string email { get; set; }
+        public string login {get; set;}
+        public string senha { get; set; }
+        public string confirmarSenha { get; set; }
+    }
+}
